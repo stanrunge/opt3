e619d4f45b9dac184f7b1d47a99c8f0fdaa58fa2
Stan <stanrunge03@gmail.com>
2022-12-18 14:14:12 +0100

    Initial commit

---
+++ src/main/java/dev/stanrunge/opt3/Opt3Application.java
@@ -0,0 +1,13 @@
+package dev.stanrunge.opt3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Opt3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Opt3Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/stanrunge/opt3/Opt3ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.stanrunge.opt3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Opt3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
